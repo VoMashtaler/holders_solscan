5ca4437bbbc2cf0b40a7d792c5ab6b2925619193
VoShoom <mashtalerv.p@gmail.com>
2023-02-15 03:57:49 +0200

    +aurora

--- aurora.js
+++ aurora.js
@@ -1,19 +1,19 @@
 //https://explorer.fuse.io/api?module=token&action=getTokenHolders&contractaddress=0x5dd8015cec49f4db01fd228f688bf30337d3e0a9&offset=300
-const decimalsAurora = 18;
-const pageSizeAurora = 10000;
-const pageNumberAurora = 0;
-const baseUrlAurora = `https://explorer.mainnet.aurora.dev/api?module=token&action=getTokenHolders&`;
+const decimalsCronos = 18;
+const pageSizeCronos = 10000;
+const pageNumberCronos = 0;
+const baseUrlCronos = `https://cronos.org/explorer/api?module=token&action=getTokenHolders&`;
 
 async function downloadHolders() {
-    const tokenAddress = document.querySelector("#tokenAddressInputAurora").value;
-    const url = `${baseUrlAurora}contractaddress=${tokenAddress}&page=0&offset=${pageSizeAurora}`;
+    const tokenAddress = document.querySelector("#tokenAddressInputCronos").value;
+    const url = `${baseUrlCronos}contractaddress=${tokenAddress}&page=0&offset=${pageSizeCronos}`;
   
     const response = await fetch(url);
     const data = await response.json();
     if (data.status === '1') {
       const holders = data.result.map((holder) => ({
         address: holder.address,
-        balance: (holder.value / 10 ** decimalsAurora).toFixed(2),
+        balance: (holder.value / 10 ** decimalsCronos).toFixed(2),
       }));
   
       const csvData = holders
@@ -22,14 +22,14 @@ async function downloadHolders() {
       const encodedUri = encodeURI("data:text/csv;charset=utf-8," + csvData);
       const link = document.createElement("a");
       link.setAttribute("href", encodedUri);
-      link.setAttribute("download", "_Aurora.csv");
+      link.setAttribute("download", "_Cronos.csv");
       link.click();
     } else {
       console.error('Error:', data.message);
     }
   }
   
-  document.querySelector("#downloadButtonAurora").addEventListener("click", downloadHolders);
+  document.querySelector("#downloadButtonCronos").addEventListener("click", downloadHolders);
   
   
 
